6da6874c670817c8397792cabc2399ed1be3128f
Crissky <pg2006pe@hotmail.com>
2022-07-22 22:59:31 -0300

    Aula 4 parte 2

--- language-api/src/main/java/br/com/crissky/language/api/Language.java
+++ language-api/src/main/java/br/com/crissky/language/api/Language.java
@@ -1,9 +1,19 @@
 package br.com.crissky.language.api;
 
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "teste_java")
 public class Language {
+    @Id
+    private String id;
     private String title;
     private String image;
     private int ranking;
+
+    public String getId() {
+        return id;
+    }
 
     public String getTitle() {
         return title;
@@ -15,6 +25,10 @@ public class Language {
 
     public int getRanking() {
         return ranking;
+    }
+
+    public Language() {
+
     }
 
     public Language(String title, String image, int ranking) {
--- language-api/src/main/java/br/com/crissky/language/api/LanguageController.java
+++ language-api/src/main/java/br/com/crissky/language/api/LanguageController.java
@@ -2,24 +2,26 @@ package br.com.crissky.language.api;
 
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class LanguageController {
-    private List<Language> languageList = List.of(
-            new Language(
-                    "Java",
-                    "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/java/java_256x256.png",
-                    1),
-            new Language(
-                    "PHP",
-                    "https://raw.githubusercontent.com/abrahamcalf/programming-languages-logos/master/src/php/php_256x256.png",
-                    2));
+    @Autowired
+    private LanguageRepository repository;
 
-    @GetMapping(value = "/preferred-language")
-    public String processingPreferredLanguage() {
-        return "Oi, Java!!!";
+    @GetMapping(value = "/languages")
+    public List<Language> getLanguageList() {
+        List<Language> languageList = repository.findAll();
+        return languageList;
     }
 
+    @PostMapping(value = "/languages")
+    public Language saveLanguage(@RequestBody Language language) {
+        Language savedLanguage = repository.save(language);
+        return savedLanguage;
+    }
 }
